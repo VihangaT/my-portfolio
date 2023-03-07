f2de8872db4854b631a1a77603f25abd7c2c635c
vihanga <liyanage.vihanga99@gmail.com>
2023-03-07 22:26:32 +0530

    completed the portfolio tab

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,10 @@
-import {React,useState} from 'react'
+import {React,useState,useEffect} from 'react'
 import PortfolioList from '../portfolioList/PortfolioList'
 import "./portfolio.scss"
+import {featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio} from "../../data.js"
 
 export default function Portfolio() {
-
+    const [data, setdata] = useState([])
     const [selected, setSelected] = useState("featured")
     const list = [
         {
@@ -27,35 +28,45 @@ export default function Portfolio() {
             title: "Content",
           }
     ]
+
+    useEffect(() => {
+        switch(selected){
+            case "featured":
+                setdata(featuredPortfolio);
+                break;
+            case "web":
+                setdata(webPortfolio);
+                break;
+            case "mobile":
+                setdata(mobilePortfolio);
+                break;
+            case "design":
+                setdata(designPortfolio);
+                break;
+            case "content":
+                setdata(contentPortfolio);
+                break; 
+            default:
+                setdata(featuredPortfolio)     
+        }
+    },[selected])
+
     return (
         <div className="portfolio" id="portfolio">
         <h1>Portfolio</h1>
         <ul>
             {list.map((item) => (
-                <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+                <PortfolioList key={item.id} title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
             ))}
         </ul>
         <div className="container">
-            <div className="item">
-                <img src="https://www.coderus.com/wp-content/uploads/fly-images/996776/different-types-of-software-coderus-branded-image-700x9999.jpg" alt="" />
-                <h3>Banking App</h3>
+        {data.map((d) =>(
+            <div className="item" key={d.id}>
+                <img src={d.img} alt={d.title} />
+                <h3>{d.title}</h3>
             </div>
-            <div className="item">
-                <img src="https://www.coderus.com/wp-content/uploads/fly-images/996776/different-types-of-software-coderus-branded-image-700x9999.jpg" alt="" />
-                <h3>Banking App</h3>
-            </div>
-            <div className="item">
-                <img src="https://www.coderus.com/wp-content/uploads/fly-images/996776/different-types-of-software-coderus-branded-image-700x9999.jpg" alt="" />
-                <h3>Banking App</h3>
-            </div>
-            <div className="item">
-                <img src="https://www.coderus.com/wp-content/uploads/fly-images/996776/different-types-of-software-coderus-branded-image-700x9999.jpg" alt="" />
-                <h3>Banking App</h3>
-            </div>
-            <div className="item">
-                <img src="https://www.coderus.com/wp-content/uploads/fly-images/996776/different-types-of-software-coderus-branded-image-700x9999.jpg" alt="" />
-                <h3>Banking App</h3>
-            </div>
+        ))}
+
         </div>
         
         </div>
