b1a8d96e25cef34b144decff79af6358ac0da902
vihanga <liyanage.vihanga99@gmail.com>
2023-03-14 00:07:10 +0530

    started to work on the slider

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -3,6 +3,31 @@ import "./work.scss"
 export default function Work() {
     return (
         <div className="work" id="work">
+        <div className="slider">
+            <div className="container">
+                <div className="item">
+                    <div className="left">
+                        <div className="leftContainer">
+                            <div className="imgContainer">
+                                <img src="assets/mobile.png" alt=""/>
+                            </div>
+                            
+                        <h2>Title</h2>
+                        <p>This character description generator will generate imply click again to get a new one.</p>
+                        <span>Prodjects</span>
+                        </div>
+                    </div>
+                    <div className="right">
+                    <img
+                    src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  alt=""
+                />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="assets/arrow.png" className="arrow left" alt="" />
+        <img src="assets/arrow.png" className="arrow right" alt="" />
         </div>
     )
 }
